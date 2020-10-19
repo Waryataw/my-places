adcbee9fbad22bc561dd1e66ec5049fcee3f64b9
Anton Waryat <waryataw@gmail.com>
2020-10-19 17:39:35 +0300

    Initial Commit

---
+++ my-places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-places
+//
+//  Created by Антон Варят on 19.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
