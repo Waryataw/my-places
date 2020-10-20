6e670c7b0e30263e05051922fd76891d760cf2ca
Anton Waryat <waryataw@gmail.com>
2020-10-20 20:45:40 +0300

    Custom Table View Cell

---
+++ my-places/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  my-places
+//
+//  Created by Антон Варят on 20.10.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
